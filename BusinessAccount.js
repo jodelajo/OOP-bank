ed72280d97e5b620cac542312d25807a610d09c2
jodelajo <jodelajo@gmail.com>
2021-02-20 13:39:59 +0100

    bonusopdracht

--- BusinessAccount.js
+++ BusinessAccount.js
@@ -1,35 +1,41 @@
+const Account = require("./Account")
 // Export your class here as module.exports = MyClass
-class BusinessAccount {
-    constructor(name, balance, creditLimit, sepaPermission) {
-        this.name = name;
-        this.balance = balance;
-        this.creditLimit = creditLimit;
-        this.sepaPermission = sepaPermission;
-        this.dailyLimit = dailyLimit
-    }
+// class BusinessAccount {
+//     constructor(name, balance, creditLimit, sepaPermission) {
+//
+//     }
 
-    makeDeposit(amount) {
-        if (amount > 0) {
-            this.balance = this.balance + amount;
+    class BusinessAccount extends Account {
+        constructor (name, balance, creditLimit, sepaPermission){
+            super();
+            this.name = name;
+            this.balance = balance;
+            this.creditLimit = creditLimit;
+            this.sepaPermission = sepaPermission;
+        }
+        makeDeposit(amount) {
+            if (amount > 0) {
+                this.balance = this.balance + amount;
+            }
+        }
+        makeWithdrawal(amount){
+            if  (amount < this.balance + this.creditLimit && amount < 20000){
+                this.balance = this.balance - amount;
+            }
+        }
+
+        sepaInvoice(amount){
+            if ( amount < 0) {
+                console.log('sorry hoor');
+            }
+            if (amount  > 0 && (this.sepaPermission === true)){
+                const transAct = (amount*0.01);
+                console.log('transaction:', transAct)
+                this.balance = this.balance + (amount - transAct);
+            }
         }
     }
-makeWithdrawal(amount){
-    if  (amount < this.balance + this.creditLimit && amount < dailyLimit){
-       this.balance = this.balance - amount;
-    }
-}
 
-    sepaInvoice(amount){
-        if ( amount < 0) {
-            console.log('sorry hoor');
-        }
-       if (amount  > 0 && (this.sepaPermission === true)){
-            const transAct = (amount*0.01);
-            console.log('transaction:', transAct)
-            this.balance = this.balance + (amount - transAct);
-        }
-    }
-}
 
-const dailyLimit = 20000;
+
 module.exports = BusinessAccount;
